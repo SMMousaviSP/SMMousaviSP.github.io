a788434cf65565d18e0bba313db0346c9f373a3d
S.Mohammad Mousavi <smmousavisp@gmail.com>
2020-11-04 02:17:43 +0330

    Add persian language

--- logical_design/js/main.js
+++ logical_design/js/main.js
@@ -18,10 +18,18 @@ $("#persian-typing").click(function () {
     $("#main-textarea").addClass("fa");
     $("#rendered-text").addClass("fa");
     $("#rendered-text").addClass("fa-rendered-text");
+    $("#navbar").addClass("fa");
+    $("#navbar-text").text("طراحی منطقی پایگاه داده");
+    $("#main-container").addClass("fa");
+    $("#main-textarea-label").text("متن خود را وارد کنید:")
 })
 
 $("#english-typing").click(function () {
     $("#main-textarea").removeClass("fa");
     $("#rendered-text").removeClass("fa");
     $("#rendered-text").removeClass("fa-rendered-text");
+    $("#navbar").removeClass("fa");
+    $("#navbar-text").text("Database Logical Design");
+    $("#main-container").removeClass("fa");
+    $("#main-textarea-label").text("Enter your text here:")
 })
